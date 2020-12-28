b55891d4a0a01fdcec82565fb3d786acfde02ccd
Alex <cigan@ua.fm>
2020-12-28 17:07:04 +0200

    Add <transition> with animation to BaseModal
    Be careful, <transition> works with first child, so that <transition> added to BaseModal, not to App where a modal has a wrapper (div)

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,8 @@
     </transition>
     <button @click="toggleText">Move text!</button>
   </div>
-  <base-modal @close="hideDialog" v-if="dialogIsVisible">
+  <!-- <base-modal @close="hideDialog" v-if="dialogIsVisible"> -->
+  <base-modal @close="hideDialog" :open="dialogIsVisible">
     <p>This is a test dialog!</p>
     <button @click="hideDialog">Close it!</button>
   </base-modal>
