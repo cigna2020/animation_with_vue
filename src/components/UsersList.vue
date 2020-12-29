772ce438636b4720b46bbd9882d9beec308e77d6
Alex <cigan@ua.fm>
2020-12-29 11:44:59 +0200

    Use <transition-group> to add an animation for a group of list items and v.-move to add css-style items that for moving

--- src/components/UsersList.vue
+++ src/components/UsersList.vue
@@ -1,9 +1,9 @@
 <template>
-  <ul>
+  <transition-group tag="ul" name="user-list">
     <li v-for="user in users" :key="user" @click="removeUser(user)">
       {{ user }}
     </li>
-  </ul>
+  </transition-group>
   <input type="tex" ref="inputUserName" />
   <button @click="addUser">Add User</button>
 </template>
@@ -18,6 +18,7 @@ export default {
   methods: {
     addUser() {
       this.users.unshift(this.$refs.inputUserName.value);
+      this.$refs.inputUserName.value = '';
     },
     removeUser(user) {
       this.users = this.users.filter(usr => usr != user);
@@ -37,5 +38,34 @@ li {
   padding: 1rem;
   border: 1px solid #ccc;
   font-size: 18px;
+}
+
+.user-list-enter-from {
+  opacity: 0;
+  transform: translateX(-50px);
+  background-color: yellow;
+}
+.user-list-enter-active {
+  transition: all 1s ease-in;
+  /* position: absolute; */
+}
+.user-list-enter-to,
+.user-list-leave-from {
+  opacity: 1;
+  transform: translateX(0);
+  background-color: none;
+}
+.user-list-move {
+  transition: all 0.8s ease;
+}
+
+.user-list-leave-to {
+  opacity: 0;
+  transform: translateX(50px);
+  background-color: yellow;
+}
+.user-list-leave-active {
+  transition: all 1s ease-out;
+  position: absolute;
 }
 </style>
